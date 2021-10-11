51b4bbf991f24f306f80778e076ce606fadf8ba3
haithem <haithem@lexicon.se>
2021-10-11 10:06:23 +0200

    first-commit

---
+++ Python/mymodule.py
@@ -0,0 +1,11 @@
+from myprog import Horse
+
+class Dog:
+    
+    def add(a,b):
+        return a + b
+    
+    def sub(a,b):
+        return a - b
+    
+    
---
+++ Python/myprog.py
@@ -0,0 +1,12 @@
+from mymodule import Dog
+
+
+class Horse:
+    
+    def add(a,b):
+        return a + b
+    
+    def sub(a,b):
+        return a - b
+    
+    
